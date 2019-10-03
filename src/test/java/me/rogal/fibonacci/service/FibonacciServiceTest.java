1b2e0d937db67eb1c35902681ca33514875ebc5d
Rodrigo Galba Miranda <rodrigogalba@gmail.com>
2019-10-02 21:00:53 -0300

    add calculate series for valid input

--- src/test/java/me/rogal/fibonacci/service/FibonacciServiceTest.java
+++ src/test/java/me/rogal/fibonacci/service/FibonacciServiceTest.java
@@ -1,8 +1,9 @@
 package me.rogal.fibonacci.service;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
@@ -39,5 +40,15 @@ class FibonacciServiceTest {
         Long result = service.calculate(null);
         Long expected = 0L;
         assertThat(expected, equalTo(result));
+    }
+
+    @Test
+    public void calculateSeries() {
+        assertThat(List.of(1L), equalTo(service.calculateSeries(1L)));
+        assertThat(List.of(1L, 1L), equalTo(service.calculateSeries(2L)));
+        assertThat(List.of(1L, 1L, 2L), equalTo(service.calculateSeries(3L)));
+        assertThat(List.of(1L, 1L, 2L, 3L), equalTo(service.calculateSeries(4L)));
+        assertThat(List.of(1L, 1L, 2L, 3L, 5L), equalTo(service.calculateSeries(5L)));
+        assertThat(List.of(1L, 1L, 2L, 3L, 5L, 8L), equalTo(service.calculateSeries(6L)));
     }
 }
