7f54385175824684513b65ddc8d32be63c1f0b33
Rodrigo Galba Miranda <rodrigogalba@gmail.com>
2019-10-11 20:22:37 -0300

    add unit test and setup mockito deps

---
+++ src/test/java/me/rogal/fibonacci/controller/FibonacciControllerMvcTest.java
@@ -0,0 +1,44 @@
+package me.rogal.fibonacci.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class FibonacciControllerMvcTest {
+
+    public static final String BASIC_CREDENTIAL_HEADER = "Basic YWRtaW46czNjcjN0";
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testCalculate() throws Exception {
+        calculateRequest(1L, List.of(1L));
+        calculateRequest(2L, List.of(1L, 1L));
+        calculateRequest(3L, List.of(1L, 1L, 2L));
+        calculateRequest(4L, List.of(1L, 1L, 2L, 3L));
+        calculateRequest(5L, List.of(1L, 1L, 2L, 3L, 5L));
+        calculateRequest(6L, List.of(1L, 1L, 2L, 3L, 5L, 8L));
+    }
+
+    private void calculateRequest(Long inputNumber, List<Long> expected) throws Exception {
+        mockMvc.perform(get("/api/calculate/{number}", inputNumber)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .header("authorization", BASIC_CREDENTIAL_HEADER))
+                .andExpect(status().isOk())
+                .andExpect(content().json(expected.toString()));
+    }
+}
--- src/test/java/me/rogal/fibonacci/controller/FibonacciControllerTest.java
+++ src/test/java/me/rogal/fibonacci/controller/FibonacciControllerTest.java
@@ -1,44 +1,50 @@
 package me.rogal.fibonacci.controller;
 
+import me.rogal.fibonacci.service.FibonacciService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest
-@AutoConfigureMockMvc
-public class FibonacciControllerTest {
+@ExtendWith(MockitoExtension.class)
+@RunWith(MockitoJUnitRunner.class)
+class FibonacciControllerTest {
 
-    public static final String BASIC_CREDENTIAL_HEADER = "Basic YWRtaW46czNjcjN0";
-    @Autowired
-    private MockMvc mockMvc;
+    @Mock
+    private FibonacciService service;
+
+    @InjectMocks
+    private FibonacciController controller;
 
     @Test
-    void testCalculate() throws Exception {
-        calculateRequest(1L, List.of(1L));
-        calculateRequest(2L, List.of(1L, 1L));
-        calculateRequest(3L, List.of(1L, 1L, 2L));
-        calculateRequest(4L, List.of(1L, 1L, 2L, 3L));
-        calculateRequest(5L, List.of(1L, 1L, 2L, 3L, 5L));
-        calculateRequest(6L, List.of(1L, 1L, 2L, 3L, 5L, 8L));
+    void testCalculate() {
+        Long number = 1L;
+        when(service.calculateSeries(number)).thenReturn(List.of(number));
+
+        ResponseEntity<List<Long>> response = controller.calculate(number);
+
+        verify(service, only()).calculateSeries(number);
+        assertTrue(response.getStatusCode().is2xxSuccessful());
+        assertEquals(List.of(number), response.getBody());
     }
 
-    private void calculateRequest(Long inputNumber, List<Long> expected) throws Exception {
-        mockMvc.perform(get("/api/calculate/{number}", inputNumber)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .header("authorization", BASIC_CREDENTIAL_HEADER))
-                .andExpect(status().isOk())
-                .andExpect(content().json(expected.toString()));
+    @Test
+    void testNumberValidationOnCalculate() {
+        ResponseEntity<List<Long>> response = controller.calculate(null);
+
+        verify(service, never()).calculateSeries(any());
+        assertTrue(response.getStatusCode().isError());
+        assertNull(response.getBody());
     }
 }
