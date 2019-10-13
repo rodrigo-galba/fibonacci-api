69d0fc77c7d418a65af34d1eb59ca75309cb91b3
Rodrigo Galba Miranda <rodrigogalba@gmail.com>
2019-10-13 01:40:58 -0300

    rename app. Remove custom exception handler.

--- src/main/java/me/rogal/fibonacci/Application.java
+++ src/main/java/me/rogal/fibonacci/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FibonacciApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FibonacciApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
